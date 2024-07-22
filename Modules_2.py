07e79b9ae0cfc82bd311f1ac186b9abde7fe9e5d
KRISHNA-663 <krish7cbe@gmail.com>
2024-07-22 17:19:46 +0530

    python

---
+++ Modules_2.py
@@ -0,0 +1,7 @@
+# Accessing the code from another file
+
+import Modules_add,Modules_sub # Name of the file that contains the functino add()
+ 
+
+print(Modules_add.add(9,13))
+print(Modules_sub.subb(15,13))
