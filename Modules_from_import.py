ca406e8e285c7789a6417654f7d1a6739e428494
KRISHNA-663 <krish7cbe@gmail.com>
2024-07-22 17:20:13 +0530

    python

---
+++ Modules_from_import.py
@@ -0,0 +1,11 @@
+from Modules_sub import subb
+
+print(subb(10,3))
+
+# from Modules_add import add,mul
+from Modules_add import * # Imports all the functions in the file
+
+print(mul(7,7))
+
+print(n)
+print(st) #Krishna
